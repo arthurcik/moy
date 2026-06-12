21995222282aea8871e23834d194a59841265a93
ArthurCik <ArthurCik@yahoo.com>
2026-06-13 00:21:11 +0300

    rename sql

--- src/server/scripts/kitt/sql/character_kitt_instance_veteran_status.sql
+++ src/server/scripts/kitt/sql/character_kitt_instance_veteran_status.sql
@@ -2,7 +2,7 @@ CREATE TABLE IF NOT EXISTS `character_kitt_instance_veteran_status` (
   `id` int(10) unsigned NOT NULL COMMENT 'Instance ID',
   `isVeteran` tinyint(3) unsigned NOT NULL DEFAULT '1',
   PRIMARY KEY (`id`),
-  CONSTRAINT `FK_instance_veteran_status_instance` 
+  CONSTRAINT `FK_character_kitt_instance_veteran_status` 
     FOREIGN KEY (`id`) 
     REFERENCES `instance` (`id`) 
     ON DELETE CASCADE 
