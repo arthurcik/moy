e7a046a397f816da4f01ee726bb9d894d038c80c
ArthurCik <ArthurCik@yahoo.com>
2026-07-11 04:17:57 +0300

    ghost ai clean

--- src/server/scripts/kitt/pvp_ai/kitt_bot_world_loader.cpp
+++ src/server/scripts/kitt/pvp_ai/kitt_bot_world_loader.cpp
@@ -178,21 +178,6 @@ bool IsPlayerInBotTracker(uint32 charGuidLow)
     }
     return false;
 }
-/*
-class LoginQueryHolder : public CharacterDatabaseQueryHolder
-{
-private:
-    uint32 m_accountId;
-    ObjectGuid m_guid;
-public:
-    LoginQueryHolder(uint32 accountId, ObjectGuid guid)
-        : m_accountId(accountId), m_guid(guid) {
-    }
-    ObjectGuid GetGuid() const { return m_guid; }
-    uint32 GetAccountId() const { return m_accountId; }
-    bool Initialize();
-};
-*/
 
 void PornesteTotiBotii()
 {
