a58e8e74c0049c9fe53a8cc0a57643e9503ffba5
ArthurCik <ArthurCik@yahoo.com>
2026-08-08 17:07:46 +0300

    add reward for BG->AV

--- src/server/scripts/kitt/kitt_battleground_reward.cpp
+++ src/server/scripts/kitt/kitt_battleground_reward.cpp
@@ -8,6 +8,8 @@
 #include "ScriptMgr.h"
 #include "Player.h"
 #include "Config.h"
+#include "Battleground.h"
+#include "Chat.h"
 #include "Log.h"
 #include <unordered_map>
 #include <mutex>
@@ -17,6 +19,8 @@ namespace
     static uint32 kittBattlegroundRewardEnable = 0;
     static uint32 KillCount = 10;
     static const uint32 AVitemReward = 20560; // AV mark
+    uint32 static const itemMarkOfHonor = 43589; // Wintergrasp Mark
+    uint32 static const itemStoneKeeperShard = 43228; // Stone Keeper's Shard
 
     std::unordered_map<ObjectGuid, uint32> avKillsMap;
     std::mutex avKillsMutex;
@@ -105,9 +109,42 @@ public:
     }
 };
 
+class kitt_battleground_rewards_player : public PlayerScript
+{
+public:
+    kitt_battleground_rewards_player() : PlayerScript("kitt_battleground_rewards_player") {}
+
+    void OnBattlegroundEnd(Player* player, Battleground* bg, uint32 winner) override
+    {
+        if (!player || !bg)
+            return;
+
+        if (player->GetBattlegroundTypeId() != BATTLEGROUND_AV)
+            return;
+
+        if (bg->isArena())
+            return;
+
+        if (winner != ALLIANCE && winner != HORDE)
+            return;
+
+        // Ambele functii returneaza acum acelasi tip de valori: ALLIANCE (469) sau HORDE (67)
+        uint32 playerBgTeam = player->GetBGTeam();
+
+        // Comparam direct valorile reale de factiuni
+        if (playerBgTeam == winner)
+        {
+            player->AddItem(itemMarkOfHonor, 1);
+            player->AddItem(itemStoneKeeperShard, 3);
+
+            ChatHandler(player->GetSession()).SendSysMessage("Ai castigat meciul! Ai primit 1x Wintergrasp Mark si 3x Stone Keeper's Shard.");
+        }
+    }
+};
 
 void AddSC_kitt_battleground_reward()
 {
     new kitt_battleground_reward_config();
     new kitt_battleground_reward_av();
+    new kitt_battleground_rewards_player();
 }
