15850243e8ec1b7326a8ab5e33c2d86e73936e59
ArthurCik <ArthurCik@yahoo.com>
2026-09-07 21:27:11 +0300

    test scripts

--- src/server/scripts/kitt/sql/kitt_ghost_command.sql
+++
@@ -1,9 +0,0 @@
-INSERT INTO `rbac_permissions`(`id`, `name`) VALUES (50001, 'Kitt Command for player');
-INSERT INTO `rbac_permissions`(`id`, `name`) VALUES (50002, 'Kitt command for gm rank 5');
-INSERT INTO `rbac_permissions`(`id`, `name`) VALUES (50003, 'Kitt command for gm rank 9');
-
-INSERT INTO `rbac_linked_permissions`(`id`, `linkedId`) VALUES (195, 50001);
-INSERT INTO `rbac_linked_permissions`(`id`, `linkedId`) VALUES (9900, 50002);
-INSERT INTO `rbac_linked_permissions`(`id`, `linkedId`) VALUES (192, 50002);
-INSERT INTO `rbac_linked_permissions`(`id`, `linkedId`) VALUES (192, 50003);
-
